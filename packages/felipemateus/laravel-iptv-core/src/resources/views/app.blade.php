e6f270579770ac433b006ed191c5cff5f5c03775
Felipe Mateus <eu@felipemateus.com>
2025-04-26 12:48:03 -0300

    chore:update laravel 12 (#18)

--- packages/felipemateus/laravel-iptv-core/src/resources/views/app.blade.php
+++ packages/felipemateus/laravel-iptv-core/src/resources/views/app.blade.php
@@ -82,7 +82,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; <a href="https://felipemateus.com/">Felipe Mateus</a> 2021</span>
+                        <span>Copyright &copy; <a href="https://felipemateus.com/">Felipe Mateus</a> 2021- 2025</span>
                     </div>
                 </div>
             </footer>
